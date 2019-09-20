ac3aeca94b244313f0aaf0069c5af6a5b235e624
facutorraca <ftorraca@fi.uba.ar>
2019-09-20 00:42:46 -0300

    medio gil medio boludo, ya paso el sercom

--- Code/main.cpp
+++ Code/main.cpp
@@ -9,7 +9,7 @@
 #define NO_FILE "-"
 
 int main(int argc, char *argv[]) {
-    if (argc != 5) {
+    if (argc != 6) {
         std::cerr << "Parametros invalidos" << '\n';
         return ERROR;
     }
@@ -20,20 +20,19 @@ int main(int argc, char *argv[]) {
     //argv[4] -> i_filename
     //argv[5] -> o_filename
 
-    Compressor compressor(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]));
+    Compressor compressor(atoi(argv[2]), atoi(argv[3]), atoi(argv[1]));
 
     if (strncmp(NO_FILE, argv[4], 1) != 0) {
         if (compressor.set_input_file(argv[4]) == ERROR) {
             std::cerr << "El archivo no se pudo abrir o no existe" << '\n';
             return ERROR;
-        };
-
+        }
     }
     if (strncmp(NO_FILE, argv[5], 1) != 0) {
         if (compressor.set_output_file(argv[5]) == ERROR) {
             std::cerr << "El archivo no se pudo crear" << '\n';
             return ERROR;
-        };
+        }
     }
 
     compressor.compress();
