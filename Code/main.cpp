771d8eeca210c092e4d402ccc0e550952babf6bd
facutorraca <ftorraca@fi.uba.ar>
2019-09-20 00:31:48 -0300

    el otro no compilaba :P jaja

--- Code/main.cpp
+++ Code/main.cpp
@@ -9,7 +9,7 @@
 #define NO_FILE "-"
 
 int main(int argc, char *argv[]) {
-    if (arg != 5) {
+    if (argc != 5) {
         std::cerr << "Parametros invalidos" << '\n';
         return ERROR;
     }
@@ -23,14 +23,14 @@ int main(int argc, char *argv[]) {
     Compressor compressor(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]));
 
     if (strncmp(NO_FILE, argv[4], 1) != 0) {
-        if (compressor.set_input_file(i_filename) == ERROR) {
+        if (compressor.set_input_file(argv[4]) == ERROR) {
             std::cerr << "El archivo no se pudo abrir o no existe" << '\n';
             return ERROR;
         };
 
     }
     if (strncmp(NO_FILE, argv[5], 1) != 0) {
-        if (compressor.set_output_file(o_filename) == ERROR) {
+        if (compressor.set_output_file(argv[5]) == ERROR) {
             std::cerr << "El archivo no se pudo crear" << '\n';
             return ERROR;
         };
